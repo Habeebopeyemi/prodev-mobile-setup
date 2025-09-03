2bd932aabb93f367111a226eb929a0e781e827e1
Habeebopeyemi <opeyemihabeeb4@gmail.com>
2025-09-03 22:11:54 +0100

    feat: update

--- prodev-mobile-app-0x00/app-example/app/(tabs)/index.tsx
+++ prodev-mobile-app-0x00/app-example/app/(tabs)/index.tsx
@@ -3,12 +3,13 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View
+      className="flex-1 items-center justify-center bg-blue-500"
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
       }}>
-      <Text>First App Created</Text>
+      <Text className="text-white text-lg font-bold">First App Created</Text>
     </View>
   );
 }
